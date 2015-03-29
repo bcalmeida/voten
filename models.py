cc74864258b50cad3e397f9182e8ac8e56fd0c14
Bruno Almeida <bruno.correia.almeida@gmail.com>
2015-03-29 16:32:36 -0400

    Add users to model and migrate database
    The new data was the added on psql:
    	=# INSERT INTO users VALUES (1, 'admin', 'ad@min.com', 'admin');
    	=# UPDATE posts SET author_id=1;
    
    The admin is the only user, and has made all the posts.

--- models.py
+++ models.py
@@ -1,4 +1,8 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
 from app import db
+
 
 class BlogPost(db.Model):
 
@@ -7,10 +11,31 @@ class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
+    author_id = db.Column(db.Integer, ForeignKey('users.id'))
 
     def __init__(self, title, description):
         self.title = title
         self.description = description
 
     def __repr__(self):
-        return "<%s - %s>" % (self.title, self.description)
+        return "<title: %s --- description: %s>" % (self.title, self.description)
+
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    posts = relationship("BlogPost", backref="author")
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return '<user: %s>' % self.name
+
