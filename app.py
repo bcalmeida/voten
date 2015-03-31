36c6ebea7f1602c1eceeb8122e9499299b5d33e1
Bruno Almeida <bruno.correia.almeida@gmail.com>
2015-03-31 01:11:18 -0400

    Add REST endpoint for getting poll

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import jsonify, abort, make_response
 from flask.ext.sqlalchemy import SQLAlchemy
 from functools import wraps
 import os
@@ -55,6 +56,27 @@ def logout():
     session.pop('logged_in', None)
     flash("You were just logged out!")
     return redirect(url_for('welcome'))
+
+## RESTful API endpoints ##
+@app.route('/poll/<int:poll_id>', methods=['GET'])
+def get_poll(poll_id):
+    poll = db.session.query(Poll).filter_by(id=poll_id).first()
+    if poll is None:
+        abort(404)
+
+    candidates = db.session.query(Candidate).filter_by(poll_id=poll_id).all()
+    candidates_jsons = [{'description': candidate.description, 'votes': candidate.votes} for candidate in candidates]
+
+    poll_json = {
+        'description': poll.description,
+        'candidates': candidates_jsons
+    }
+
+    return jsonify({'poll': poll_json})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not Found'}), 404)
 
 # Start server
 if __name__ == '__main__':
