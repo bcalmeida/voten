50f17d346084c40f1b2d879b1ec41868af1ad8aa
Bruno Almeida <bruno.correia.almeida@gmail.com>
2015-04-02 07:51:27 -0400

    Add style to /create page using Bootstrap and validate form

--- client/app/js/controllers.js
+++ client/app/js/controllers.js
@@ -31,22 +31,48 @@ controller('createController', function ($scope, $location, pollsAPIservice) {
     $scope.description = "";
     $scope.choices = [{'position': 1}, {'position': 2}, {'position': 3}];
     $scope.buttonDisabled = false;
+    $scope.valid = true;
 
     $scope.addNewChoice = function() {
         var position = $scope.choices.length + 1;
         $scope.choices.push({'position': position});
     };
 
+    $scope.removeChoice = function() {
+        var position = $scope.choices.length + 1;
+        if ($scope.choices.length > 2) {
+            $scope.choices.pop();
+        }
+    };
+
     // TODO: Do not add empty choices. Or do not allow empty choices?
     $scope.createPoll = function() {
+        // Check if form is valid
+        $scope.valid = true;
+        if ($scope.description == "") {
+            $scope.valid = false;
+        }
+        for (var i = 0; i < $scope.choices.length; i++) {
+            if (!('description' in $scope.choices[i]) || ($scope.choices[i].description == "")){
+                $scope.valid = false;
+            }
+        }
+        if (!$scope.valid) {
+            return;
+        }
+
+        // Form is valid
+        // Disable button
         $scope.buttonDisabled = true;
 
+        // Create poll
         var poll = {'description': $scope.description};
         poll.candidates = []
         for (var i = 0; i < $scope.choices.length; i++) {
             poll.candidates.push({'description': $scope.choices[i].description});
         }
 
+        // Send poll
         pollsAPIservice.createPoll(poll).
             success(function (data) {
                 $location.path('/poll/' + data.poll_id);
